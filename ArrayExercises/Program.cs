3d16eb9c22736218112f7e5e005ee6cb90da46cc
Richard Carino <richard.carino17@gmail.com>
2020-06-06 04:20:16 +1000

    to turn in

--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -6,7 +6,30 @@ namespace ArrayExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Arrays!");
+            int[] numArr = { 1, 1, 2, 3, 5, 8 };
+
+            Console.WriteLine("All numbers printed:");
+            for (int i = 0;  i < numArr.Length; i++)
+            {
+                if (i == numArr.Length - 1)
+                {
+                    Console.WriteLine(numArr[i] + "\n");
+                    break;
+                }
+                Console.WriteLine(numArr[i]);
+
+            }
+
+            Console.WriteLine("Odd numbers printed");
+            for (int i = 0; i < numArr.Length; i++)
+            {
+                if (numArr[i]%2 != 0)
+                {
+                    Console.WriteLine(numArr[i]);
+
+                }
+
+            }
         }
     }
 }
--- StringExercises/Program.cs
+++ StringExercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace StringExercises
 {
@@ -6,7 +7,19 @@ namespace StringExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Strings!");
+            string words = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
+
+            string[] wordsSplit = words.Split(' ');
+
+            for (int i = 0; i < wordsSplit.Length; i++)
+            {
+                if (wordsSplit[i].Contains(".")|| wordsSplit[i].Contains(",")) 
+                { 
+                    wordsSplit[i] = wordsSplit[i].Substring(0, wordsSplit[i].Length - 1); 
+                }
+            }
+
+            Console.WriteLine(string.Join(",", wordsSplit));
         }
     }
 }
